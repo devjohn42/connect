8b61d273f1b2337e9f5b78f4780050f4fb278db2
devjohn42 <devjohncb@gmail.com>
2025-10-02 18:54:15 -0300

    :hammer: refactor: creates a specific folder for all routes

---
+++ src/http/subscribe-to-event.ts
@@ -0,0 +1,29 @@
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export const subscribeToEvent: FastifyPluginAsyncZod = async (app) => {
+  app.post('/subscription', {
+  schema: {
+      summary: 'Subscribes someone to the event',
+      tags: ['subscription'],
+      description: 'A larger description of what this route is about',
+      body: z.object({
+        name: z.string(),
+        email: z.email()
+      }),
+      response: {
+        201: z.object({
+          name: z.string(),
+          email: z.email()
+        })
+      },
+    }
+  }, async (request, reply) => {
+    const {name, email} = request.body
+
+    return reply.status(201).send({
+      name,
+      email
+  })
+})
+}
