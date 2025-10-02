8c8a86887e5a60c781ad4ed9be3e91b14c366252
devjohn42 <devjohncb@gmail.com>
2025-10-01 22:52:55 -0300

    :gear: config: adds fastify-cors for security and zod for validation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,36 @@
 import fastify from "fastify"
+import { fastifyCors } from '@fastify/cors'
+import { validatorCompiler, serializerCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from "zod"
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.get('/server', (request, reply) => {
-  return 'Server Running'
+app.register(fastifyCors, {
+  origin: 'http://localhost:3333'
+})
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.post('/subscription', {
+  schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.email()
+      }),
+      response: {
+        201: z.object({
+          name: z.string(),
+          email: z.email()
+        })
+      },
+    }
+  }, async (request, reply) => {
+    const {name, email} = request.body
+
+    return reply.status(201).send({
+      name,
+      email
+  })
 })
 
 app.listen({port: 3333}).then(() => {
