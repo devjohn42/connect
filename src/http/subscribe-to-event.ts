83fc6e7bd0ccb3ded36f65c0c9914c2c6994b457
devjohn42 <devjohncb@gmail.com>
2025-10-03 12:29:00 -0300

    :gear: config: creates eslint config

--- src/http/subscribe-to-event.ts
+++ src/http/subscribe-to-event.ts
@@ -1,29 +1,33 @@
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import { z } from "zod";
+import { z } from 'zod'
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
 export const subscribeToEvent: FastifyPluginAsyncZod = async (app) => {
-  app.post('/subscription', {
-  schema: {
-      summary: 'Subscribes someone to the event',
-      tags: ['subscription'],
-      description: 'A larger description of what this route is about',
-      body: z.object({
-        name: z.string(),
-        email: z.email()
-      }),
-      response: {
-        201: z.object({
+  app.post(
+    '/subscription',
+    {
+      schema: {
+        summary: 'Subscribes someone to the event',
+        tags: ['subscription'],
+        description: 'A larger description of what this route is about',
+        body: z.object({
           name: z.string(),
-          email: z.email()
-        })
+          email: z.email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.email(),
+          }),
+        },
       },
-    }
-  }, async (request, reply) => {
-    const {name, email} = request.body
+    },
+    async (request, reply) => {
+      const { name, email } = request.body
 
-    return reply.status(201).send({
-      name,
-      email
-  })
-})
+      return reply.status(201).send({
+        name,
+        email,
+      })
+    },
+  )
 }
